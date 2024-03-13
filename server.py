86a79c34903302c8919c3afb41d95f63929a3f4e
Robert04 <bobby.toth@gmail.com>
2024-03-12 22:56:59 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+import requests
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/fetch-listings', methods=['GET'])
+def fetch_equipment_listings():
+    # Get rarity from request parameters, default to 'diamond' if not provided
+    rarity = request.args.get('rarity', 'diamond')
+    cardType = request.args.get('type', 'mlb_card')
+    # Construct the API request URL
+    api_url = 'https://mlb23.theshow.com/apis/listings.json'
+    params = {
+        'type': cardType,
+        'rarity': rarity,
+        'sort': 'best_sell_price',
+        'order': 'desc'
+    }
+
+    # Make the request to the external API
+    response = requests.get(api_url, params=params)
+
+    # Return the JSON response from the API to the frontend
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(debug=True)
